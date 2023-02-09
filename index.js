7d7b4dcd35ca412b9308292e5c57ff7416da5425
Kaushik Das <kaushik.das@codeclouds.in>
2023-02-09 17:34:06 +0530

    updated index.js file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! My name is Kaushik Das  ');
+  res.send(' Hello, My name is Kaushik Das  ');
 });
 
 var server = app.listen(3000, function () {
